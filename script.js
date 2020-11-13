65a7d13b0ee6ad56cc759cdf5b3c5f2f6ed906d0
fkibria <fkibria02@gmail.com>
2020-11-12 19:41:56 -0500

    adding dropdown to html and api calls to js

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+$(document).ready(function(){
+   
+            
+  $('select').formSelect();
+
+  function searchAlcoholicOrNot() {
+      var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list"
+      $.ajax({
+          type:"GET",
+          url: queryURL
+      }).then(function(response){
+          console.log(response);
+          var alcoholicOrNot = response.drinks;
+
+      })}
+
+  function searchGlass() {
+      var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list"
+      $.ajax({
+          type:"GET",
+          url: queryURL
+      }).then(function(response){
+          console.log(response);
+      })}
+
+  function searchIngredient() {
+      var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+      $.ajax({
+          type:"GET",
+          url: queryURL
+      }).then(function(response){
+          console.log(response);
+      })}
+
+  
+
+      searchAlcoholicOrNot();
+      searchGlass();
+      searchIngredient();
+      
+
+
+});
+
+
 //Functionality for modal button (favourites)
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
