60956042912daa81a294b4dc61b15d3f1c832492
fkibria <fkibria02@gmail.com>
2020-11-17 17:25:45 -0500

    took out extra timezone p tag and added extra api key

--- timezoneUTC.js
+++ timezoneUTC.js
@@ -158,11 +158,13 @@ type: "GET",
 url: queryURL,
 data:{
 
-  'apiKey' : '1b51bf787f9348f4a954d4b7ba72ade1',
+'apiKey' : '1b51bf787f9348f4a954d4b7ba72ade1',
 
 // 'apiKey' : 'af77de5fb3e042849edd4c67482147f8',
 
 // 'apiKey' : 'ea34208be1244f88b4f11f251e8c3d60',
+
+// 'apiKey' : 'fcf30077888745fcb8b2623c69d753f7',
 
 },
 cache: false,
